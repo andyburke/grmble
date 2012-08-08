b2f9550bace46010ee14034788d964aa7217cdab
Andy Burke <aburke@bitflood.org>
2012-08-07 17:11:37 -0700

    WIP reorg, API updates

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ process.on( 'uncaughtException', function ( error ) {
 
 var express = require( 'express' );
 var sha1 = require( 'sha1' );
+var events = require( 'events' );
 
 var dbHost = process.env[ 'MONGO_HOST' ] != null ? process.env[ 'MONGO_HOST' ] : 'localhost';
 var dbPort = process.env[ 'MONGO_PORT' ] != null ? process.env[ 'MONGO_PORT' ] : 27017;
@@ -43,17 +44,85 @@ io.configure( function(){
     ] );
 });
 
-var apiModules = [
-    require( './api/Sessions.js' ),
-    require( './api/Users.js' ),
-    require( './api/Rooms.js' ),
-    require( './api/Messages.js' ),
-    require( './api/Socket.js' )
+app.eventEmitter = new events.EventEmitter();
+
+app.subsystems = [
+    // api
+    require( './api/1.0/Info.js' ),
+    require( './api/1.0/Sessions.js' ),
+    require( './api/1.0/Users.js' ),
+    require( './api/1.0/Socket.js' ),
+    require( './api/1.0/Messages.js' ),
+    require( './api/1.0/Rooms.js' )
 ];
 
-for ( var moduleIndex = 0; moduleIndex < apiModules.length; ++moduleIndex )
-{
-    apiModules[ moduleIndex ].bind( app, io );
+app.GetURLs = function() {
+    var result = {};
+    for ( var subsystem in app.subsystems )
+    {
+        if ( app.subsystems[ subsystem ].GetURLs )
+        {
+            result = extend( result, app.subsystems[ subsystem ].GetURLs() );
+        }
+    }
+    
+    return result;
 }
 
-app.listen( process.env.TURNS_PORT || 8000 );
+app.use( express.static( __dirname + '/client/web' ) );
+
+// prebindings for any use statements
+for ( var index = 0; index < app.subsystems.length; ++index )
+{
+    if ( typeof( app.subsystems[ index ].prebind ) != 'undefined' )
+    {
+        app.subsystems[ index ].prebind( app, __dirname );
+    }
+}
+
+app.use( express.staticCache() ); // cache our static files
+
+app.WithURLs = function( request, ref, postprocess ) {
+    function AddURLs( obj ) {
+        var result = obj._doc ? obj._doc.clone() : obj.clone();
+        result.urls = {};
+        for ( var subsystem in app.subsystems )
+        {
+            if ( app.subsystems[ subsystem ].GetURLs )
+            {
+                result.urls = extend( result.urls, app.subsystems[ subsystem ].GetURLs( obj ) );
+            }
+        }
+        
+        for ( var urlKey in result.urls )
+        {
+            if ( result.urls[ urlKey ][ 0 ] == '/' )
+            {
+                result.urls[ urlKey ] = 'http://' + request.headers.host + result.urls[ urlKey ];
+            }
+        }
+
+        return postprocess ? postprocess( result ) : result;
+    }
+    
+    if ( ref instanceof Array )
+    {
+        var result = [];
+        for ( var index = 0; index < ref.length; ++index )
+        {
+            result.push( AddURLs( ref[ index ] ) );
+        }
+        return result;
+    }
+    else
+        return AddURLs( ref );
+}
+
+for ( var index = 0; index < app.subsystems.length; ++index )
+{
+    app.subsystems[ index ].bind( app, io );
+}
+
+var port = process.env[ 'GRUMBLE_PORT' ] || 8000;
+console.log( 'Server loaded, listening on port ' + port + ' ...' );
+app.listen( port );
