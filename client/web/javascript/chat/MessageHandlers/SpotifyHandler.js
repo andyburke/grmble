2308d1894eb49a0aae66970a84de61c1d58abccf
Andy Burke <aburke@bitflood.org>
2012-10-10 16:10:40 -0700

    Allow matching albums.

--- client/web/javascript/chat/MessageHandlers/SpotifyHandler.js
+++ client/web/javascript/chat/MessageHandlers/SpotifyHandler.js
@@ -16,13 +16,13 @@ var SpotifyHandler = function() {
     self.Handle = function( href ) {
         var result = null;
         
-        if ( href.match( /(?:http?:\/\/)?(?:open\.)?spotify\.com\/track\/(\w*)/i ) )
+        if ( href.match( /(?:http?:\/\/)?(?:open\.)?spotify\.com\/(\w*)\/(\w*)/i ) )
         {
-            var matched = href.match( /(?:http?:\/\/)?(?:open\.)?spotify\.com\/track\/(\w*)/i );
+            var matched = href.match( /(?:http?:\/\/)?(?:open\.)?spotify\.com\/(\w*)\/(\w*)/i );
 
-            if ( matched && matched.length == 2 )
+            if ( matched && matched.length == 3 )
             {
-                result = '<iframe class="dynamic-embed" src="https://embed.spotify.com/?theme=white&view=coverart&uri=spotify:track:' + matched[1] + '" width="250" height="80" frameborder="0" allowtransparency="true"></iframe>';
+                result = '<iframe class="dynamic-embed" src="https://embed.spotify.com/?theme=white&view=coverart&uri=spotify:' + matched[ 1 ] + ':' + matched[ 2 ] + '" width="250" height="80" frameborder="0" allowtransparency="true"></iframe>';
             }
         }
        
